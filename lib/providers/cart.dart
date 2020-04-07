5f6e81d7659514abbaace7a03bd2f16b19393757
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-07 10:46:55 +0530

    added cart provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final num price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(
+    String productId,
+    num price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCardItem) => CartItem(
+                id: existingCardItem.id,
+                title: existingCardItem.title,
+                price: existingCardItem.price,
+                quantity: existingCardItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
-import '../providers/products.dart';
 
 enum filterOptions {
   Favourites,
