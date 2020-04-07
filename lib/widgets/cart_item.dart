7582093474d0f8a12624350d1378ecfb50749e60
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-07 22:36:33 +0530

    added remove cart list tile option

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,41 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id,this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      background: Container(
+        color: Theme.of(context).primaryColorLight,
+        child: Icon(
+          Icons.delete,
+          color: Theme.of(context).primaryColorDark,
+          size: 35,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: FittedBox(
-                child: Text(
-                  '\u20B9 $price',
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(4),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: FittedBox(
+                  child: Text(
+                    '\u20B9 $price',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
+              radius: 35,
+              backgroundColor: Theme.of(context).primaryColorLight,
             ),
-            radius: 35,
-            backgroundColor: Theme.of(context).primaryColorLight,
-          ),
-          title: Text(title),
-          subtitle: Text(
-            'Total: \u20B9 ${price * quantity}',
-          ),
-          trailing: Text(
-            '$quantity x',
+            title: Text(title),
+            subtitle: Text(
+              'Total: \u20B9 ${price * quantity}',
+            ),
+            trailing: Text(
+              '$quantity x',
+            ),
           ),
         ),
       ),
