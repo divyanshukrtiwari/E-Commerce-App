a6fa2ef9a954bf63e4c2af9bfbffbb22e26b3c9f
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-09 12:06:32 +0530

    added app drawer in  user products screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
 import '../providers/products.dart';
 
@@ -21,6 +22,7 @@ class UserProductScreen extends StatelessWidget {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
