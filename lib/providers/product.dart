d7f24695bd5a527770c3ebba347003be3f90cd01
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-12 22:53:23 +0530

    added updating and storing favourites on firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -10,16 +13,33 @@ class Product with ChangeNotifier {
   bool isFavourite;
 
   Product({
-    @required this.id, 
-    @required this.title, 
-    @required this.description, 
-    @required this.price, 
-    @required this.imageUrl, 
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus(){
-    isFavourite =! isFavourite;
+  void toggleFavouriteStatus() async {
+    final url = 'https://my-shop-e4082.firebaseio.com/products/$id.json';
+    final oldStatus = isFavourite;
+    isFavourite = !isFavourite;
     notifyListeners();
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavourite': isFavourite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        isFavourite = oldStatus;
+        notifyListeners();
+      }
+    } catch (error) {
+      isFavourite = oldStatus;
+      notifyListeners();
+    }
   }
 }
