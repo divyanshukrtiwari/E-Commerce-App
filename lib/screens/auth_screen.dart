5f20d2eea3bff5339f4f26d34ebcf5d2b8d57773
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-15 00:15:37 +0530

    added animatedcontainer widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -109,29 +109,29 @@ class _AuthCardState extends State<AuthCard>
   AnimationController _controller;
   Animation<Size> _heightAnimation;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 300),
-    );
-    _heightAnimation = Tween<Size>(
-      begin: Size(double.infinity, 260),
-      end: Size(double.infinity, 320),
-    ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInToLinear),
-    );
-    //_heightAnimation.addListener(() => setState((){}));
-  }
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   _controller = AnimationController(
+  //     vsync: this,
+  //     duration: Duration(milliseconds: 300),
+  //   );
+  //   _heightAnimation = Tween<Size>(
+  //     begin: Size(double.infinity, 260),
+  //     end: Size(double.infinity, 320),
+  //   ).animate(
+  //     CurvedAnimation(parent: _controller, curve: Curves.easeInToLinear),
+  //   );
+  //   //_heightAnimation.addListener(() => setState((){}));
+  // }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _controller.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  //   _controller.dispose();
+  // }
 
   void _showErrorDialog(String error) {
     showDialog(
@@ -220,18 +220,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          //height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(
-            minHeight: _heightAnimation.value.height,
-          ),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _heightAnimation.value.height,
         ),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
